1d50d66d8410418f193ba76ddd47dcf42db8ec95
Clarisse Kessab <clarisseksb@gmail.com>
2025-02-27 15:21:11 +0100

    Page Projet

--- src/app/projets/[slug]/page.tsx
+++ src/app/projets/[slug]/page.tsx
@@ -1,8 +1,7 @@
 import { Metadata } from 'next'
-
-
-
 import projets from '../../../data/projects.json'
+import { GrGroup, GrDocumentTime, GrDocument } from "react-icons/gr";
+
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface Projet {
@@ -35,22 +34,48 @@ export default async function Page({ params }: PageProps) {
     return <div>Projet non trouvé</div>
   }
 
+  const projetUrl = projet.demo;
+
+  const getButtonText = (url: string): string => {
+    if (url.startsWith("https://github.com/")) return "Voir le GitHub";
+    if (url.startsWith("https://canva.com")) return "Voir la présentation";
+    if (url.startsWith("https://notion.so/")) return "Voir le Notion";
+    if (url.startsWith("https://figma.com/")) return "Voir le Figma";
+    return "Visiter le site";
+  };
+
   return (
     <>
       <div className="projet-title">
         <h1>{projet.title}</h1>
         <p>{projet.description}</p>
         <div className="btns-projet">
-          <a href={`${projet.github}`}className="btn btn-projet"> Voir le projet</a>
+          <a href={`${projet.github}`}className="btn btn-projet"> {getButtonText(projetUrl)}</a>
           <a className="btn btn-projet-secondary"> Des questions ?</a>
         </div>
       </div>
-      <div className="blur-big-green left-10 top"></div>
+      <div className="types-projet">
+        <div className="type-projet">
+          <GrGroup className="icon-projet"/>
+          <p className="title-type-projet">Par <br/> {projet.team.length} personnes</p>
+        </div>
+        <div className="type-projet">
+          <GrDocumentTime className="icon-projet"/>
+          <p className="title-type-projet">Réalisé en <br/> {projet.duration}</p>
+        </div>
+        <div className="type-projet">
+          <GrDocument className="icon-projet"/>
+          <p className="title-type-projet">Projet <br/> {projet.projet}</p>
+        </div>
+
+
+      </div>
+      <div className="blur-big-violet left-10 top"></div>
         <div className="container-img-projet">
         <img src={projet.image} alt={`Presentation ${projet.title}`} className="image-projet" />
         </div>
         <div className="stack-section">
-        <h2 className="h2">Technologies utilisées</h2>
+        <h2 className="h2 hidden">Technologies utilisées</h2>
         <div className="stack-group">
         {projet.stack.map((stack, index) => (
           <div className="element-skill" key={index}>
@@ -60,17 +85,39 @@ export default async function Page({ params }: PageProps) {
         ))}
         </div>
         </div>
+        <div className="texts-projet">
+          <div className="brief-projet">
+          <h2 className="h2">Contexte ✨</h2>
+          <p>{projet.brief}</p>
+          </div>
+          <div className="feature-projet">
+          <h2 className="h2">Fonctionnalités ⚙️</h2>
+      <div className="blur-big-green left-10 top"></div>
+
+          {projet.features.map((feature, index) => (
+          <div className="features" key={index}>
+            <p className="feature">{feature}</p>
+          </div>
+        ))}
+          </div>
+        </div>
       <div className="photos-section">
-        <h2 className="h2">Photos</h2>
+        <h2 className="h2">Aperçu 📸</h2>
         <div className="photos-group">
         {projet.images.map((image, index) => (
           <div className="photo-projet" key={index}>
             <img src={`${image}`} alt={projet.title} className="img-projet" />
           </div>
         ))}
+      <div className="blur-little-blue left-10 top"></div>
         </div>
         </div>
-
     </>
   )
 }
+
+
+{/* <div className="btns-projet">
+          <a href={`${projet.github}`}className="btn btn-projet"> {getButtonText(projetUrl)}</a>
+          <a className="btn btn-projet-secondary"> Des questions ?</a>
+        </div> */}
