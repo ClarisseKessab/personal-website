37d4fecbfd313c6b2b4a153bfdfa138e28511386
Clarisse Kessab <clarisseksb@gmail.com>
2025-04-09 23:26:12 +0200

    Sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://clarissek-dev.fr', // 🔁 à remplacer par ton domaine
+  generateRobotsTxt: true, // ✅ génère aussi le fichier robots.txt
+  sitemapSize: 7000,
+};
